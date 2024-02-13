c17d1f0ffd3ef623539864ef50fb70ff56adf03a
Gerard Cebria <gerard.cebrialeon@luminis.eu>
2024-02-13 12:10:33 +0100

    workshop backup

---
+++ scripts/deploy_infrastructure.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace # For debugging
+
+###################
+# REQUIRED ENV VARIABLES:
+#
+# PROJECT
+# AZURE_ENV_NAME
+# AZURE_LOCATION
+# AZURE_SUBSCRIPTION_ID
+
+#####################
+# DEPLOY ARM TEMPLATE
+
+# Set account to where ARM template will be deployed to
+echo "Deploying to Subscription: $AZURE_SUBSCRIPTION_ID"
+az account set --subscription "$AZURE_SUBSCRIPTION_ID"
+
+arm_output=""
+
+# Create resource group
+resource_group_name="$PROJECT-$AZURE_ENV_NAME-rg"
+echo "Creating resource group: $resource_group_name"
+az group create --name "$resource_group_name" --location "$AZURE_LOCATION" --tags Environment="$AZURE_ENV_NAME"
+
+# By default, set all KeyVault permission to deployer
+# Retrieve KeyVault User Id
+kv_owner_object_id=$(az ad signed-in-user show --output json | jq -r '.id')
+
+
+# Validate arm template
+echo "Validating deployment"
+arm_output=$(az deployment sub validate \
+    --template-file "./infra/main.bicep" \
+    --location="$AZURE_LOCATION" \
+    --parameters resourceGroupName="${resource_group_name}" location="${AZURE_LOCATION}" environmentName="${AZURE_ENV_NAME}" \
+        principalId="${AZURE_PRINCIPAL_ID}" authClientId="${AZURE_AUTH_CLIENT_ID}" \
+    --output json)
+
+########################
+# DEPLOY ARM TEMPLATE
+
+echo "Deploying resources into $resource_group_name"
+arm_output=$(az deployment sub create \
+    --template-file "./infra/main.bicep" \
+    --location="$AZURE_LOCATION" \
+    --parameters resourceGroupName="${resource_group_name}" location="${AZURE_LOCATION}" environmentName="${AZURE_ENV_NAME}" \
+        principalId="${AZURE_PRINCIPAL_ID}" authClientId="${AZURE_AUTH_CLIENT_ID}" \
+    --output json)
+
+if [[ -z $arm_output ]]; then
+    echo >&2 "ARM deployment failed."
+    exit 1
+fi
